3e563e372eb55148516bdeac909dac74eb9203f5
Michael Kolawole <Michael.Kolawole@gmail.com>
2021-02-18 20:18:40 +0000

    cleared up boilerplate files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello World</h1>
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'Define the term algorithm',
+    answer: 'A step by step set of instructions to solve a problem'
+  },
+  {
+    id: 2,
+    question: 'Define the term abstraction',
+    answer: 'Removing unnecessary information until your left with what is essential'
+  }
+]
 
 export default App;
